9bb273e737d5b03f338f2e225e6a9c797877d66f
By73 <byterepo@gmail.com>
2014-05-02 21:49:32 -0500

    more alias

--- zsh/alias/general.zsh
+++ zsh/alias/general.zsh
@@ -1,1 +1,3 @@
 alias tmux='tmux -2'
+alias nemo='nemo $PWD'
+alias dotfiles='$HOME/.dotfiles'
--- zsh/alias/system.zsh
+++ zsh/alias/system.zsh
@@ -1,6 +1,6 @@
 alias hibernate='sudo pm-hibernate'
-alias shutdown='systemctl shutdown -i'
-alias reboot='systemctl reboot -i'
+alias shutdown='sudo systemctl shutdown -i'
+alias reboot='sudo systemctl reboot -i'
 alias rsrc='source $HOME/.zshrc'
 alias free='free -m'
 
